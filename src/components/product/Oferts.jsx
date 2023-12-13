b765d651a8a0890ad285c7c9906208bcc7c0e03e
jarvinc3 <mangomc03@gmail.com>
2023-12-13 17:54:26 -0400

    login desing

--- src/components/product/Oferts.jsx
+++ src/components/product/Oferts.jsx
@@ -184,7 +184,7 @@ export default function Oferts() {
       {/* Slider controls */}
       <button
         type="button"
-        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        className="absolute top-0 start-0 z-20 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         data-carousel-prev=""
       >
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
@@ -208,7 +208,7 @@ export default function Oferts() {
       </button>
       <button
         type="button"
-        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+        className="absolute top-0 end-0 z-20 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
         data-carousel-next=""
       >
         <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes ,Route } from "react-router-dom";
 import Dashboard from "./containers/home/Dashboard";
+import Home from "./containers/home/Home";
 import ProductDetail from "./components/product/ProductDetail";
 import Login from "./containers/account/Login";
-import ProductList from "./components/product/ProductList";
 import Cart from "./containers/cart/Cart"
 import ProductJewelery from "./components/product/ProductJewelery";
 import ProductElectronics from "./components/product/ProductElectronics";
 import ProductMen from "./components/product/ProductMen";
 import ProductWomen from "./components/product/ProductWomen";
+import SingUp from "./containers/account/SingUp";
 
 export default function App() {
   return (
@@ -15,8 +16,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/singup" element={<SingUp />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/home" element={<ProductList />} />
         <Route path="/detail" element={<ProductDetail />} />
         <Route path="/electronics" element={<ProductElectronics />} />
         <Route path="/jewelery" element={<ProductJewelery />} />
